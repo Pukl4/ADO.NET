a8d81084109051c9b16171e266ee3d261d17de67
Puki4 <nikitka2601@mail.ru>
2024-09-08 00:06:21 +0300

    Change AverageGrade double to float

--- BasicFunctions/BasicFunctions/Program.cs
+++ BasicFunctions/BasicFunctions/Program.cs
@@ -10,44 +10,38 @@ namespace BasicFunctions
         {
             var repository = new StudentRepository();
 
-            // Connected
             var studentConnected = repository.GetAllConnected();
             Console.WriteLine("Connected Model:");
             PrintStudents(studentConnected);
             Console.WriteLine("-----\n");
 
-            // Disconnected
             var studentDisconnected = repository.GetAllDisconnected();
             Console.WriteLine("Disconnected Model:");
             PrintStudents(studentDisconnected);
             Console.WriteLine("-----\n");
 
-            // Stored procedure
             var studentProcedure = repository.GetAllStoredViaProcedure();
             Console.WriteLine("Fetched via a Stored procedure:");
             PrintStudents(studentProcedure);
             Console.WriteLine("-----\n");
 
-            // Fetched by id
             var student = repository.Get(1);
             Console.WriteLine("Fetched by id:");
             PrintStudent(student);
             Console.WriteLine("-----\n");
 
-            // Student faculty name fetched via a function
             var facultyName = repository.GetFacultyNameViaFucntion(student.Id);
             Console.WriteLine("Student faculty name:");
             Console.WriteLine(facultyName);
             Console.WriteLine("-----\n");
 
-            // Add a new student
             var newStudent = new StudentModel()
             {
                 FirstName = "Edward",
                 LastName = "Brown",
                 Email = "m.f@protonmail.edu",
                 PhoneNumber = "1-683-204-3142",
-                AverageGrade = 9.2d,
+                AverageGrade = 9.2f,
                 FacultyId = 1
             };
             var newStudentId = repository.Add(newStudent);
